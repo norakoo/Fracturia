6334564972d8b5d251fb5178844cea79c70bfb6b
norakoo <nolanh76lh@gmail.com>
2025-11-28 13:56:21 +0100

    Initial commit.

---
+++ src/main/java/com/norako/fracturia/Fracturia.java
@@ -0,0 +1,17 @@
+package com.norako.fracturia;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Fracturia implements ModInitializer {
+	public static final String MOD_ID = "fracturia";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize()
+    {
+
+	}
+}
---
+++ src/main/java/com/norako/fracturia/FracturiaClient.java
@@ -0,0 +1,10 @@
+package com.norako.fracturia;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FracturiaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
